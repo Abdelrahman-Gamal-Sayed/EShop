4e7901651bb255567cf531147341abe7cc40bc06
Abdo <abdelrahman@axlr8.tech>
2021-07-10 13:19:30 +0200

    add login

---
+++ EShop.BusinessLayer/ViewModels/ProductCreateViewModel.cs
@@ -0,0 +1,32 @@
+﻿using EShop.DataLayer.Models;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace EShop.BusinessLayer.ViewModels
+{
+    public class ProductCreateViewModel
+    {
+
+        [Required]
+        [MaxLength(50, ErrorMessage = "Name cannot exceed 50 characters")]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+        [Required]
+        public decimal Price { get; set; }
+        [Required]
+        public Category? Category { get; set; }
+        public IFormFile Photo { get; set; }
+    }
+
+    public enum categories
+    {
+        TVs,
+        Laptops,
+        SoundSystems
+
+    };
+}
--- EShop.PresentationLayer/Controllers/ProductController.cs
+++ EShop.PresentationLayer/Controllers/ProductController.cs
@@ -1,9 +1,12 @@
 ﻿using EShop.BusinessLayer.ViewModels;
+using EShop.DataLayer.Models;
 using EShop.DataLayer.Repositories;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -12,10 +15,12 @@ namespace EShop.PresentationLayer.Controllers
     public class ProductController : Controller
     {
         private readonly IProductRepository _productRepository;
-
-        public ProductController(IProductRepository productRepository)
+        private readonly IWebHostEnvironment hostingEnvironment;
+        public ProductController(IProductRepository productRepository,
+                              IWebHostEnvironment hostingEnvironment)
         {
             _productRepository = productRepository;
+            this.hostingEnvironment = hostingEnvironment;
         }
         // GET: ProductController
         public ActionResult Index()
@@ -40,25 +45,54 @@ namespace EShop.PresentationLayer.Controllers
         }
 
         // GET: ProductController/Create
+        [HttpPost]
+        public IActionResult Create(ProductCreateViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+
+
+                Product product = new Product
+                {
+                    Name = model.Name,
+                    Description = model.Description,
+                    CategoryId = model.Category.Id,
+                    Price = model.Price,
+                };
+
+                _productRepository.Add(product);
+                string uniqueFileName = ProcessUplodedFile(model, product.Id);
+                return RedirectToAction("details", new { id = product.Id });
+            }
+
+            return View();
+        }
+        private string ProcessUplodedFile(ProductCreateViewModel model,int id)
+        {
+            string uniqueFileName = null;
+            if (model.Photo != null)
+            {
+      
+                string uploadsFolder = Path.Combine(hostingEnvironment.WebRootPath, "images");
+
+                uniqueFileName = id.ToString();
+                string filePath = Path.Combine(uploadsFolder, uniqueFileName);
+      
+                using (var fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    model.Photo.CopyTo(fileStream);
+                }
+
+            }
+
+            return uniqueFileName;
+        }
+        // POST: ProductController/Create
         public ActionResult Create()
         {
             return View();
         }
 
-        // POST: ProductController/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
 
         // GET: ProductController/Edit/5
         public ActionResult Edit(int id)
